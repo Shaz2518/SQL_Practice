28871a8ff9a4be96f8eacf62b179fbb29ccdc3d4
Shaz2518 <shaziyashaikh2518@outlook.com>
2026-04-26 21:25:34 +0530

    Group By and Aggregate Functions

--- GroupBy_AggregateFuncts.sql
+++ GroupBy_AggregateFuncts.sql
@@ -20,7 +20,6 @@ insert into Transactions values('April','Chennai',56);
 insert into Transactions values('May','Mumbai',43456);
 insert into Transactions values('May','Chennai',43456);
 insert into Transactions values('May','Bangalore',44);
-
 select * from Transactions;
 
 #Get sum of amount from each month/branch
@@ -38,4 +37,17 @@ select Branch, min(Amount) from Transactions group by Branch;
 #Get Average amount from each month/branch
 Select Month, avg(Amount) from Transactions group by Month;
 select Branch, avg(Amount) from Transactions group by Branch;
+
+#Gte count of transaction in each month/branch
+select Month,count(*) from Transactions group by Month;
+select Branch,count(*) from Transactions group by branch;
+
+select Month,count(*) from Transactions group by Month having count(*)>=2;
+
+
+#Get total sum of amount for each month in Mumbai and Bangalore;
+select Branch,sum(Amount) from Transactions 
+	where Branch='Mumbai' or Branch='Bangalore' 
+		group by Branch 
+			having sum(Amount)<20000;
 
